482341bf3294810effea1a3103244103ca3f00a6
SJ-cody <snehjain2809@gmail.com>
2024-09-20 00:31:16 +0530

    Add files via upload

---
+++ class101/Activity3.py
@@ -0,0 +1,13 @@
+number = int(input('Enter a number: '))
+
+if number >  15:
+
+   print('The number you entered is greater than 15.')
+   print('I am in if block')
+
+else:
+   print('i am in else block ')
+   print('The number you entered is less than 15.')
+
+print('not in any block')
+
